7898e6adba2faf5e7665381e245a071b0b0dd854
Jeffrey Kaufmann <atriums111@gmail.com>
2022-01-03 17:51:57 -0700

    Cleaning UI

--- public/frontpage.js
+++ public/frontpage.js
@@ -256,6 +256,27 @@ function valueChangedPartyFrame()
   }
 }
 
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
+
 function generateJson()
 {
   options.NAMEPLATE_HIDE_ALLY=String(allAllyCheck);
